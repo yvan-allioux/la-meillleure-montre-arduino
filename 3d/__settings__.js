d1c42aa3f95208b4d88b916544e024c60a656870
yvan-allioux <yvan.allioux.pro@gmail.com>
2022-12-19 16:49:45 +0100

    3d local

---
+++ 3d/__settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1131597.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+SCRIPTS = [ 45989369, 45989356, 45989377, 45989389, 46033661, 46033711, 46036168, 46227334, 46322830 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/46322195/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/46322196/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/46322194/1/ammo.js', 'preload' : true},
+];
